fff9bba8c649c0c827bbcd3234065a6132feb344
Imamudin Naseem <naseem1992@gmail.com>
2019-10-14 18:15:47 +0530

    fixing type

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -2,6 +2,8 @@ declare module "toggler-react" {
     import * as React from "react";
     import {IProps, IState} from "toggler-react/src/components/Toggler/Toggler";
     class Toggler extends React.Component<IProps, IState> {
+        static Field: React.FC;
+        static Popover: React.FC;
     }
     export default Toggler;
 }
