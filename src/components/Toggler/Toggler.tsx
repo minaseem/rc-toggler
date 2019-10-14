da9b5e20ab8a76598ad63ee0ab19be850c531d76
Imamudin Naseem <naseem1992@gmail.com>
2019-10-14 17:47:58 +0530

    fix types

--- src/components/Toggler/Toggler.tsx
+++ src/components/Toggler/Toggler.tsx
@@ -5,14 +5,14 @@ import clickEvent from "./../../helpers/clickEvent";
 import hasParentChildRelationship from "./../../utils/hasParentChildRelationship";
 import { IObject } from "../../types/types";
 
-interface IProps {
+export interface IProps {
   popoverVisible: boolean;
   onToggle: (a: boolean) => void;
   overrideClass?: string;
   children: ReactNode;
 }
 
-interface IState {
+export interface IState {
   showPopover: boolean;
 }
 
@@ -30,7 +30,7 @@ function Popover() {
   return null;
 }
 
-export default class Toggle extends Component<IProps, IState> {
+export default class Toggler extends Component<IProps, IState> {
   static Field: React.FC = Field;
   static Popover: React.FC = Popover;
   private field: React.RefObject<HTMLDivElement>;
@@ -94,13 +94,13 @@ export default class Toggle extends Component<IProps, IState> {
     const { showPopover } = this.state;
     const field = (children as JSX.Element[]).find(
       (child: JSX.Element) =>
-        child.type === Toggle.Field ||
-        child.type.displayName === Toggle.Field.name
+        child.type === Toggler.Field ||
+        child.type.displayName === Toggler.Field.name
     );
     const popover = (children as JSX.Element[]).find(
       (child: JSX.Element) =>
-        child.type === Toggle.Popover ||
-        child.type.displayName === Toggle.Popover.name
+        child.type === Toggler.Popover ||
+        child.type.displayName === Toggler.Popover.name
     );
     return (
       <div className={`toggler ${overrideClass}`}>
@@ -120,6 +120,6 @@ export default class Toggle extends Component<IProps, IState> {
 }
 
 
-Toggle.defaultProps = {
+Toggler.defaultProps = {
   overrideClass: ""
 };
