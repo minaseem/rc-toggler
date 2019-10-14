927d963fe3f6df44f90fa3613dce2cee4df0ef70
Imamudin Naseem <naseem1992@gmail.com>
2019-10-14 12:05:03 +0530

    example

---
+++ example/src/App.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import {Toggler} from 'aaa/toggler'
+import './App.css';
+
+function App() {
+  const [popoverVisibile, setpopoverVisibile] = useState(false);
+  return (
+    <div className="App">
+    hello
+      <Toggler popoverVisible={popoverVisibile} onToggle={setpopoverVisibile}>
+        <Toggler.Field><div>Field</div></Toggler.Field>
+        <Toggler.Popover>
+          <div>Popover</div>
+        </Toggler.Popover>
+      </Toggler>
+    </div>
+  );
+}
+
+export default App;
