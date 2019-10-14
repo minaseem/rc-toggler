2e64c35b0e45369c582747c0a5d860209f0d64c8
Imamudin Naseem <naseem1992@gmail.com>
2019-10-14 15:32:53 +0530

    adding type

---
+++ lib/main.d.ts
@@ -0,0 +1,7 @@
+declare module "toggler-react" {
+    import * as React from "react";
+    import {IProps, IState} from "toggler-react/src/components/Toggler/Toggler";
+    class Toggler extends React.Component<IProps, IState> {
+    }
+    export default Toggler;
+}
