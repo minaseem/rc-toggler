3a5122d30842861224c1a4e04fb83a02059b17d6
Imamudin Naseem <naseem1992@gmail.com>
2019-10-15 12:15:30 +0530

    fix(ts): fix import

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -1,6 +1,6 @@
 declare module "toggler-react" {
     import * as React from "react";
-    import {IProps, IState} from "toggler-react/src/components/Toggler/Toggler";
+    import {IProps, IState} from "../src/components/Toggler/Toggler";
     class Toggler extends React.Component<IProps, IState> {
         static Field: React.FC;
         static Popover: React.FC;
